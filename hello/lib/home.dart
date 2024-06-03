7ecd56a48849cb3f9cc1317dc62a6421aa1c2456
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-06-03 12:03:44 -0300

    ListViewBuilder

--- hello/lib/home.dart
+++ hello/lib/home.dart
@@ -6,9 +6,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text("Hello World")),
-      bottomNavigationBar: BottomNavBar(),
+    const gibberishList = ["asasassdasd", "asdafasf", "kjnekvndssio"];
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text("Hello World"),
+              const Divider(),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: gibberishList.length,
+                itemBuilder: (context, index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text(gibberishList[index])],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
